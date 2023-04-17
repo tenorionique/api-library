800442114e5520314a2f2a5a72e90ac068252c47
monique <monique.sa@raizen.com>
2023-04-17 08:08:52 -0300

    aula 1 completa

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Sobre a editora',
+    '/sobre': 'Sobre o projeto'
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end(rotas[req.url]);
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
