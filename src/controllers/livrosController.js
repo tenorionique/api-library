6dd252548fce2df3896604a8af30679b5dc610c2
monique <monique.sa@raizen.com>
2023-04-24 18:05:27 -0300

    aula 5 completa

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,9 +3,25 @@ import livros from '../models/Livro.js'
 class LivroController {
 
     static listarLivrvos = (req, res) => {
-        livros.find((err, livros) => {
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
             res.status(200).json(livros);
         })
+    }
+
+        static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
+                if(err) {
+                    res.status(400).send({message: `${err.message} - id do livro não localizado`})
+                } else {
+                    res.status(200).send(livros);
+                }
+            })
     }
 
     static cadastrarLivro = (req, res) => {
@@ -32,17 +48,6 @@ class LivroController {
         })
     }
 
-    static listarLivroPorId = (req, res) => {
-        const id = req.params.id;
-
-        livros.findById(id, (err, livros) => {
-            if(err) {
-                res.status(400).send({message: `${err.message} - id do livro não localizado`})
-            } else {
-                res.status(200).send(livros);
-            }
-        })
-    }
 
     static excluirLivro = (req, res) => {
         const id = req.params.id;
@@ -53,6 +58,14 @@ class LivroController {
             } else {
                 res.status(500).send({message: err.message})
             }
+        })
+    }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
 
